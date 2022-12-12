915df962db55b4adc6990d75838b19b109495375
Bartosz Puchyrski <bartosz.puchyrski@gmail.com>
2022-12-12 19:04:18 +0100

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,7 @@ const input = document.querySelector("#name-input");
 const text = document.querySelector("#name-output");
 
 input.addEventListener("input", (event) => {
-  text.textContent = event.currentTarget.value;
+  text.textContent = event.currentTarget.value.trim();
   if (text.textContent === "") {
     text.textContent = "Anonymus";
   }
